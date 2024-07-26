1cced2c50611523b1daef92b763b0efd455248fb
AishuPola <aishwarya.pola@proclink.com>
2024-07-26 17:42:30 +0530

    recipedetails

---
+++ src/app/recipedetails/recipedetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipedetails',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipedetails.component.html',
+  styleUrl: './recipedetails.component.scss'
+})
+export class RecipedetailsComponent {
+
+}
