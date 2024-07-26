e0dcc6c7d647087d471e3abb37bac48fff9b2309
AishuPola <aishwarya.pola@proclink.com>
2024-07-26 17:04:36 +0530

    addrecipes added

---
+++ src/app/add-recipe/add-recipe.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AllrecipesComponent } from '../allrecipes/allrecipes.component';
+
+@Component({
+  selector: 'app-add-recipe',
+  standalone: true,
+  imports: [FormsModule, AllrecipesComponent],
+  templateUrl: './add-recipe.component.html',
+  styleUrl: './add-recipe.component.scss',
+})
+export class AddRecipeComponent {
+  @Input() recipeadd: any;
+  image = '';
+  title = '';
+  category = '';
+
+  addrecipe() {
+    this.recipeadd.push({
+      poster: this.image,
+      name: this.title,
+      rating: this.category,
+      // summary: this.summary,
+    });
+  }
+}
