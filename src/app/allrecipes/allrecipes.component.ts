3718bd7da1837d0a6713b9d4e9bf5475daa469fb
srujan1306 <srujan.pothu@proclink.com>
2024-07-26 15:43:30 +0530

    recipe added

---
+++ src/app/allrecipes/allrecipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allrecipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './allrecipes.component.html',
+  styleUrl: './allrecipes.component.scss'
+})
+export class AllrecipesComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AllrecipesComponent } from './allrecipes/allrecipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AllrecipesComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'recipes';
