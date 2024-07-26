02748339299fa533d5048a788154bdc64f3d453c
srujan1306 <srujan.pothu@proclink.com>
2024-07-26 16:15:43 +0530

    all recipes added

--- src/app/allrecipes/allrecipes.component.ts
+++ src/app/allrecipes/allrecipes.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RecipeServiceService } from '../recipe-service.service';
 
 @Component({
   selector: 'app-allrecipes',
   standalone: true,
   imports: [],
   templateUrl: './allrecipes.component.html',
-  styleUrl: './allrecipes.component.scss'
+  styleUrl: './allrecipes.component.scss',
 })
 export class AllrecipesComponent {
-
+  @Input() everyrecipe: any;
+  // allrecipes: any;
+  // constructor(public RecipeServiceService: RecipeServiceService) {
+  //   this.recipe_list = this.RecipeServiceService.recipes;
+  // }
 }
