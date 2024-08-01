3e53ffd1a91dbb3577766387f77d3e1bddf7e52d
AishuPola <aishwarya.pola@proclink.com>
2024-08-01 18:25:36 +0530

    validations added

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -8,12 +8,13 @@ import {
 } from '@angular/forms';
 
 import { RecipeServiceService } from '../recipe-service.service';
-import { MatFormField } from '@angular/material/form-field';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { Newrecipe } from '../recipe';
 import { Router } from '@angular/router';
 import { Recipe } from '../app.component';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-recipe',
@@ -24,6 +25,10 @@ import { Recipe } from '../app.component';
     MatInput,
     MatButtonModule,
     ReactiveFormsModule,
+    MatError,
+    MatSelect,
+    MatLabel,
+    MatOption,
   ],
   templateUrl: './add-recipe.component.html',
   styleUrl: './add-recipe.component.scss',
@@ -31,14 +36,14 @@ import { Recipe } from '../app.component';
 export class AddRecipeComponent {
   recipeForm: FormGroup;
   recipeadd!: Array<Recipe>;
-  image = '';
-  title = '';
-  category = '';
-  rating = '';
-  summary = '';
-  ingredients = '';
-  timeToComplete = '';
-  procedure = '';
+  // image = '';
+  // title = '';
+  // category = '';
+  // rating = '';
+  // summary = '';
+  // ingredients = '';
+  // timeToComplete = '';
+  // procedure = '';
 
   // constructor(public RecipeServiceService: RecipeServiceService) {
   //   this.recipeadd = this.RecipeServiceService.recipes;
@@ -78,17 +83,17 @@ export class AddRecipeComponent {
     });
   }
   addrecipe() {
-    let recipeadd: Newrecipe = {
-      image: this.image,
-      title: this.title,
-      category: this.category,
-      rating: this.rating,
-      // summary: this.summary,
-      summary: this.summary,
-      ingredients: this.ingredients,
-      timeToComplete: this.timeToComplete,
-      procedure: this.procedure,
-    };
+    // let recipeadd: Newrecipe = {
+    //   image: this.image,
+    //   title: this.title,
+    //   category: this.category,
+    //   rating: this.rating,
+    //   // summary: this.summary,
+    //   summary: this.summary,
+    //   ingredients: this.ingredients,
+    //   timeToComplete: this.timeToComplete,
+    //   procedure: this.procedure,
+    // };
     if (this.recipeForm.value) {
       let recipeadd: Newrecipe = this.recipeForm.value;
 
@@ -96,5 +101,14 @@ export class AddRecipeComponent {
         this.router.navigate(['search']);
       });
     }
+  }
+  get title() {
+    return this.recipeForm.get('title');
+  }
+  get image() {
+    return this.recipeForm.get('image');
+  }
+  get rating() {
+    return this.recipeForm.get('rating');
   }
 }
